cf7562c790e67e1889b3c5a21994f62b5ad7a008
bordoray <bordoray@gmail.com>
2025-06-24 22:18:57 +0900

    Fix

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -6,7 +6,7 @@ from qgis.PyQt.QtGui import QIcon
 
 def test_registered(qgis_app: QgsApplication, provider: str):
     registory = QgsApplication.processingRegistry()
-    provider = registory.providerById("")
+    provider = registory.providerById("mojxmlloader")
     assert provider is not None
     assert len(provider.name()) > 0
     assert isinstance(provider.icon(), QIcon)
